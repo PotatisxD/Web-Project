fe643a2a70dc218aee28f372cc4faf7e2560fd56
Kenny <kenny_lee_1@hotmail.com>
2022-03-29 16:57:07 +0200

    template, index

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include('template.php');
+$content = <<<END
+<h1>Welcome to this website</h1>
+<p>
+This is gonna be a Smart Home GUY
+</p>
+END;
+echo $navigation;
+echo $content;
+?>
---
+++ template.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Smart Home Gui</title>
+<meta charset="utf-8">
+<link rel="stylesheet" property="stylesheet" type="text/css" href="css/stylesheet.css">
+</head>
+
+<?php
+session_name('Website');
+session_start();
+$host = "localhost";
+$user = "felcar21"; 
+$pwd = "";
+$db = "felcar21_db";
+$mysqli = new mysqli($host, $user, $pwd, $db);
+$navigation = <<<END
+<nav>
+ <a href="index.php">Home</a>
+ <a href="about.php">About</a>
+ <a href="products.php">Products</a>
+ <a href="login.php">LOGIN</a>
+END;
+
+if (isset($_SESSION['userId']))
+{
+ $navigation .= <<<END
+ <a href="add_product.php">Add product</a>
+ <a href="register.php">Reigster new user</a>
+ <a href="logout.php">Logout</a>
+ Logged in as {$_SESSION['username']}
+END;
+}
+ $navigation .= '</nav>';
+
+?>
