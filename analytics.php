e5c45e0ef58254680ed5883e2d5fda783190573e
PotatisxD <70508792+PotatisxD@users.noreply.github.com>
2022-04-07 13:09:25 +0200

    Fixat tables i analytics

--- analytics.php
+++ analytics.php
@@ -1,6 +1,8 @@
 <?php
 require_once('template.php');
+function createIpTable(){
 
+}
 $browsercount = array();
 $browsernames = array();
 $query1 = <<<END
@@ -26,9 +28,14 @@ array_push($browsernames, $row->BrowserName);
 $formatbrowsercount = implode(", ", $browsercount);
 $formatbrowsernames = "'" . implode ( "', '", $browsernames) . "'";
 
+
+
 $content = <<<END
-<div class="chart-container" style="position: float-right; height:40vh; width:80vw">
-  <canvas id="myChart"></canvas>
+<div class="chart-container" style="position:absolute; height:300px; width:400px; top:50px; right:350px;">
+<canvas id="myChart"></canvas>
+</div>
+<div id="iptable">
+
 </div>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script>
@@ -62,4 +69,88 @@ $content = <<<END
 END;
 echo $navigation;
 echo $content;
+
+echo '<table border=1px;>';
+echo "<tr border=1px;>";
+echo "<td>IP Adress </td>";
+echo "<td>Number of occurrences </td>";
+echo "</tr>";
+
+$query1 = <<<END
+SELECT DISTINCT project_Log.IPAdress
+FROM project_Log
+END;
+$result1 = $mysqli->query($query1);
+if ($result1->num_rows > 0) {
+while ($row = $result1->fetch_object()) {
+$query2 = <<<END
+SELECT COUNT(*)
+FROM project_Log
+WHERE IPAdress="{$row->IPAdress}"
+END;
+
+$result2 = $mysqli->query($query2);
+$temp = mysqli_fetch_array($result2);
+$count = $temp['COUNT(*)'];
+
+echo "<tr border=1px;>";
+echo "<td> {$row->IPAdress} </td>";
+echo "<td> {$count} </td>";
+echo "</tr>";
+}
+}
+echo "</table>";
+
+echo '<table border=1px;>';
+echo "<tr border=1px;>";
+echo "<td>User</td>";
+echo "<td>Page</td>";
+echo "<td>Date</td>";
+echo "</tr>";
+
+
+$query1 = <<<END
+SELECT UserID, LogID
+FROM project_UserLog
+ORDER BY UserID
+END;
+
+$result1 = $mysqli->query($query1);
+if ($result1->num_rows > 0) {
+while ($row = $result1->fetch_object()) {
+$query2 = <<<END
+SELECT PageID, TimeStamp
+FROM project_Log
+WHERE LogID="{$row->LogID}"
+END;
+$result2 = $mysqli->query($query2);
+$temp = $result2->fetch_object();
+
+$query3 = <<<END
+SELECT project_Page.Page
+FROM project_Page
+WHERE PageID="{$temp->PageID}"
+END;
+
+$result3 = $mysqli->query($query3);
+$temp2 = $result3->fetch_object();
+$current_Page = $temp2->Page;
+
+$query4 = <<<END
+SELECT project_User.UserName
+FROM project_User
+WHERE UserID="{$row->UserID}"
+END;
+$result4 = $mysqli->query($query4);
+$temp3 = $result4->fetch_object();
+
+echo "<tr border=1px;>";
+echo "<td> {$temp3->UserName} </td>";
+echo "<td> {$current_Page} </td>";
+echo "<td> {$temp->TimeStamp} </td>";
+echo "</tr>";
+}
+}
+echo "</table>";
+
 ?>
