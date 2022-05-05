c579f204328919ef1ec4b6595aa09cadbd15d517
Tedrap <tedrap21@student.hh.se>
2022-05-05 14:24:14 +0200

    finare login

--- login.php
+++ login.php
@@ -27,24 +27,22 @@ $content = <<<END
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sign in</title>
-
     <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/sign-in/">
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-   
+    <link rel="stylesheet" href="media/style.css">
     <link href="sidebars.css" rel="stylesheet">
   </head>
-  <body class="text-center">
+  <body class="bg-dark">
     <main>
     <div class="form-signin align-items-center">
-      <form method="POST" action="login.php">
-        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+      <form class="login-form" method="POST" action="login.php">
+        <h1 class="h3 mb-3 fw-normal text-light">Please sign in</h1>
         <div class="form-floating">
-          <input type="text" class="form-control" id="username" name="username">
+          <input type="text" class="form-control" id="username" name="username" required>
           <label for="username">Username</label>
         </div>
         <div class="form-floating">
-          <input type="password" class="form-control" id="password" name="password">
+          <input type="password" class="form-control" id="password" name="password" required>
           <label for="password">Password</label>
         </div>
         <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
