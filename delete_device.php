8544411c2fb08367a87075b94da432c777a2837c
Tedrap <tedrap21@student.hh.se>
2022-05-12 16:02:15 +0200

    kommentar och fix?

--- delete_device.php
+++ delete_device.php
@@ -1,7 +1,7 @@
 <?php
 // Ted.R Code
 include_once('template.php');
-// if the GET variable and session is set, a query will run
+// if the GET variable and session is set, the query will run
 if (isset($_GET['DeviceID']) and isset($_SESSION['userId'])) {
 // The query deletes the device entry in the database with DeviceID in GET
 $query = <<<END
