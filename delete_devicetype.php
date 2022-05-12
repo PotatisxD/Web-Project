35c09dd5be0a9a12bd878436b96056ad61fabe04
Tedrap <tedrap21@student.hh.se>
2022-05-12 15:51:25 +0200

    kommentarer

--- delete_devicetype.php
+++ delete_devicetype.php
@@ -1,6 +1,8 @@
 <?php
 include_once('template.php');
+// if the GET variable and session is set, a query will run
 if (isset($_GET['DeviceTypeID']) and isset($_SESSION['userId'])) {
+// The query deletes the devicetype entry in the database with DeviceTypeID in GET
 $query = <<<END
 DELETE FROM project_DeviceType
 WHERE DeviceTypeID='{$_GET['DeviceTypeID']}'
--- delete_room.php
+++ delete_room.php
@@ -1,6 +1,8 @@
 <?php
 include_once('template.php');
+// if the GET variable and session is set, a query will run
 if (isset($_GET['RoomID']) and isset($_SESSION['userId'])) {
+// The query deletes the room entry in the database with RoomID in GET
 $query = <<<END
 DELETE FROM project_Room
 WHERE RoomID='{$_GET['RoomID']}'
