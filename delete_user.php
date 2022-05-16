daedcc1b14be916733189d994e103fba8625eab2
Kenny <kenny_lee_1@hotmail.com>
2022-05-16 15:00:31 +0200

    quickfix delete_user.php

--- delete_user.php
+++ delete_user.php
@@ -1,9 +1,9 @@
 <?php
 // Kenny.L (Quang) Code, inspiration from lab 7 (delete_products.php).
+include_once('template.php');
 // Checks if the user is a logged in user/admin
 if (isset($_GET['id']) and isset($_SESSION['userId'])) {
 // Deletes using DELETE SQL statement.
-include_once('template.php');
 $query = <<<END
 DELETE FROM project_User
 WHERE UserID = '{$_GET['id']}'
