8693d2aaa4190099ba3f555f78df09fbf2b01293
Kenny <kenny_lee_1@hotmail.com>
2022-05-24 20:00:14 +0200

    Possible to delete users again

--- delete_user.php
+++ delete_user.php
@@ -5,18 +5,11 @@ include_once('template.php');
 if (isset($_GET['id']) and isset($_SESSION['userId'])) {
 // Query that deletes using DELETE SQL statement.
 $query = <<<END
-SELECT * FROM project_Admin
-END;
-$res = $mysqli->query($query);
-if($res->num_rows > 1)
-{
-$query = <<<END
 DELETE FROM project_User
 WHERE UserID = '{$_GET['id']}'
 END;
 $mysqli->query($query);
 }
 header('Location:users.php');
-}
 echo $navigation;
 ?>
